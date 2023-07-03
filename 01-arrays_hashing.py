6e9bebafa37e57944e5fbb4d1eae1e19d362248e
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-07-02 23:12:18 -0300

    Update 01-arrays_hashing.py

--- 01-arrays_hashing.py
+++ 01-arrays_hashing.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/contains-duplicate/
 """
+217. Contains Duplicate
 Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
 Example 1:
@@ -74,3 +75,52 @@ class Solution(object):
             countS[s[i]] = 1 + countS.get(s[i], 0)
             countT[t[i]] = 1 + countT.get(t[i], 0)
         return countS == countT
+
+
+
+
+
+# https://leetcode.com/problems/two-sum/
+"""
+1. Two Sum
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+ 
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+"""
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        prevMap = {}  # val -> index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+         
