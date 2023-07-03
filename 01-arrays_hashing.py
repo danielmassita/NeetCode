6992316479ae76186dd79b69564b05fd73eb49bc
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-07-02 23:08:17 -0300

    Update 01-arrays_hashing.py

--- 01-arrays_hashing.py
+++ 01-arrays_hashing.py
@@ -2,24 +2,19 @@
 """
 Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
- 
-
 Example 1:
-
 Input: nums = [1,2,3,1]
 Output: true
-Example 2:
 
+Example 2:
 Input: nums = [1,2,3,4]
 Output: false
-Example 3:
 
+Example 3:
 Input: nums = [1,1,1,3,3,4,3,2,4,2]
 Output: true
  
-
 Constraints:
-
 1 <= nums.length <= 105
 -109 <= nums[i] <= 109
 """
@@ -36,3 +31,46 @@ class Solution(object):
                 return True
             hashset.add(n)
         return False
+
+
+
+
+
+# https://leetcode.com/problems/valid-anagram/
+"""
+242. Valid Anagram
+
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+
+Constraints:
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+
+Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+"""
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
